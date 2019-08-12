af57caa29dad8a2142bd32a8f45b826c33ab948b
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-08-12 15:48:47 -0500

    Created Solution, WebAPI project, Scaffolded Identity, and Added Roles.

---
+++ src/LiveTunes.WebAPI/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using LiveTunes.WebAPI.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LiveTunes.WebAPI.Areas.Identity.IdentityHostingStartup))]
+namespace LiveTunes.WebAPI.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ src/LiveTunes.WebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiveTunes.WebAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
