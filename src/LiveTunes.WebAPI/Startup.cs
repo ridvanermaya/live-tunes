9ecc60dc9abd70e38337d9a7c757b94706ca67bc
Alexander Lee <alexandermlee10@gmail.com>
2019-08-13 10:02:21 -0500

    added AddHttpClient service and Eventcontroller

--- src/LiveTunes.WebAPI/Startup.cs
+++ src/LiveTunes.WebAPI/Startup.cs
@@ -44,6 +44,7 @@ namespace LiveTunes.WebAPI
                 .AddRoles<IdentityRole>()
                 .AddDefaultUI(UIFramework.Bootstrap4)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddHttpClient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
