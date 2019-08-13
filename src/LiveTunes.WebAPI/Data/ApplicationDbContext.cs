96ca8608c60f6e2e6453d4c7d67f4bd998e30cec
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-08-13 08:21:38 -0500

    Changed model name MusicPreferences to MusicPreference and Added DBSets.

--- src/LiveTunes.WebAPI/Data/ApplicationDbContext.cs
+++ src/LiveTunes.WebAPI/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using LiveTunes.WebAPI.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,6 +6,15 @@ namespace LiveTunes.WebAPI.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<BusinessProfile> BusinessProfiles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<MusicPreference> MusicPreferences { get; set; }
+        public DbSet<Survey> Surveys { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
