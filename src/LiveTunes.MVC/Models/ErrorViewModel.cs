92cf09e68202e086857e2641ebd7cf8a51a70fa7
Ridvan Ermaya <ridvanermaya@gmail.com>
2019-08-13 10:54:35 -0500

    Created new MVC Project

---
+++ src/LiveTunes.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LiveTunes.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
