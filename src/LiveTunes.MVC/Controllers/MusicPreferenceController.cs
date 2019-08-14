7b8496dbb92553ffbe1a632e0d978055b4c9d8b2
Vincent Mackay <mackv42@gmail.com>
2019-08-14 12:29:04 -0500

    music preference controller no longer breaks in constructor

--- src/LiveTunes.MVC/Controllers/MusicPreferenceController.cs
+++ src/LiveTunes.MVC/Controllers/MusicPreferenceController.cs
@@ -36,8 +36,8 @@ namespace LiveTunes.MVC.Controllers
         public async void GetSongs(string genre)
         {
             //client.DefaultRequestHeaders.Add(new MediaTypeWithQualityHeaderValue("jsonp"));
-            HttpResponseMessage response = await client.GetAsync("http://itunes.apple.com/search?term=Jack+Johnson?callback=?");
-            suggestedSongs = await response.Content.ReadAsAsync<String>();
+            //HttpResponseMessage response = await client.GetAsync("http://itunes.apple.com/search?term=Jack+Johnson?callback=?");
+            //suggestedSongs = await response.Content.ReadAsAsync<String>();
         }
 
         //Main View Where it Goes thru songs based on genres they like
