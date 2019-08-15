8c7613a9635b8ae1fcfccc89f482ac834ddb79d9
Vincent Mackay <mackv42@gmail.com>
2019-08-14 21:12:48 -0500

    wrote Ajax to get all comments for an event and to show up on event details page

---
+++ src/LiveTunes.MVC/wwwroot/js/comment.js
@@ -0,0 +1,17 @@
+﻿
+function getComments(EventId) {
+    var comments = [];
+    $.ajax({
+        type: "GET",
+        url: "https://localhost:44303/comment/List/" + EventId,
+        success: function (data) {
+            console.log("got a response");
+            comments = data;
+            console.log(data);
+            var commentsDiv = document.getElementById("comments");
+            for (var i = 0; i < comments.length; i++) {
+                commentsDiv.innerHTML += comments[i].commentText + "<br/>";
+            }
+        }
+    });
+}
