0cb9175cfb1e7e3ae1e7cf4cb76bfe78bb3ae398
Alexander Lee <alexandermlee10@gmail.com>
2019-08-14 20:02:26 -0500

    git pull

--- src/LiveTunes.MVC/Controllers/EventController.cs
+++ src/LiveTunes.MVC/Controllers/EventController.cs
@@ -35,14 +35,12 @@ namespace LiveTunes.MVC.Controllers
         {
             try
             {
-              // var result = await client.GetStringAsync("https://www.eventbriteapi.com/v3/events/search?location.address=vancovuer&location.within=10km&expand=venue&token=" + EventbriteAPIToken.Token);
-
-               // var x = JsonConvert.DeserializeObject(result);
-
+                var result = await client.GetStringAsync("https://www.eventbriteapi.com/v3/events/search?location.address=vancovuer&location.within=10km&expand=venue&token=" + EventbriteAPIToken.Token);
+                var deserializedResults = JsonConvert.DeserializeObject(result);    
             }
             catch (HttpRequestException e)
             {
-                Debug.WriteLine(e.Message);
+                
             }
         }
 
